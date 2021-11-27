265e64597695d6513e45dd5f8c2204c53d24fbda
MohitBaruniya <95136570+MohitBaruniya@users.noreply.github.com>
2021-11-27 22:35:08 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,90 @@
+/* $(document).ready(function(){
+    $('.food-slider').slick({
+       autoplay:true,
+      slidesToShow:3,
+      slidesToScroll:1,
+      prevArrow:".prev-btn",
+      nextArrow:".next-btn",
+      responsive:[
+         {
+            breakpoint:992,
+            settings:{
+             slidesToShow:2,
+            }
+         },
+         {
+          breakpoint:768,
+          settings:{
+           slidesToShow:1,
+          }
+       }
+      ]
+ 
+    });
+ 
+    $('.nav-trigger').click(function(){
+       $('.site-content-wrapper').toggleClass('scaled');
+    })
+ });
+$(document).ready(function(){
+      $('.foods-slider').slick({
+          slidesToShow:3,
+      });
+  }); */
+/*   $(document).ready(function(){
+   $('.food-slider').slick({
+      autoplay:true,
+     slidesToShow:3,
+     slidesToScroll:1,
+     prevArrow:".prev-btn",
+     nextArrow:".next-btn",
+     responsive:[
+        {
+           breakpoint:992,
+           settings:{
+            slidesToShow:2,
+           }
+        },
+        {
+         breakpoint:768,
+         settings:{
+          slidesToShow:1,
+         }
+      }
+     ]
+
+   });
+
+   $('.nav-trigger').click(function(){
+      $('.site-content-wrapper').toggleClass('scaled');
+   })
+}); */
+$(document).ready(function(){
+   $('.foods-slider').slick({
+      autoplay:true,
+     slidesToShow:3,
+     slidesToScroll:1,
+     prevArrow:".pre-btn",
+     nextArrow:".nxt-btn",
+     responsive:[
+        {
+           breakpoint:992,
+           settings:{
+            slidesToShow:2,
+           }
+        },
+        {
+         breakpoint:768,
+         settings:{
+          slidesToShow:1,
+         }
+      }
+     ]
+
+   });
+
+   $('.nav-trigger').click(function(){
+      $('.site-content-wrapper').toggleClass('scaled');
+   })
+});
+   
